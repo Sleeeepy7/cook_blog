dee582443d03309a513f68b624bfbb0f7bcbea5d
Вадим <90385669+Sleeeepy7@users.noreply.github.com>
2021-09-20 16:53:56 +0300

    Update settings.py

--- cook_blog/cook_blog/settings.py
+++ cook_blog/cook_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_^k$*@=ede04)+_z+3xrt#2l_-9w#4p$q08f^p1rkez(v1x$s'
+SECRET_KEY = '' #секретный ключ находится у вторы --> Sleeeepy7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
